ae0f086554fb1f343e1eb62b6917bf51e59cd517
hariiieeee <hariharan2760@gmail.com>
2025-09-14 09:18:16 +0530

    Sep14

--- SixPhrase/CircularLinkedList.java
+++ SixPhrase/CircularLinkedList.java
@@ -1,3 +1,81 @@
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+    }
+}
+
 public class CircularLinkedList {
-    
+    static Node head;
+    static Node tail;
+
+    static void createNewList(int[] arr){
+        for(int i: arr){
+            Node newNode  = new Node(i);
+            if(head == null){
+                head = newNode;
+                newNode.next = head;
+                tail = head;
+                continue;
+            }
+
+            tail.next = newNode;
+            newNode.next = head;
+            tail = newNode; 
+        }
+    }
+
+    static void printList(){
+        Node current = head;
+        while(current != tail){
+            System.err.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println(current.data + " -> ");
+    }
+
+    static void insertAtHead(int data){
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+            tail.next = head;
+            return;
+        }
+
+        newNode.next = head;
+        tail.next = newNode;
+        head = newNode;
+    }
+
+    static void insertAtTail(int data){
+        Node newNode = new Node(data);
+
+        if(tail == null){
+            head = newNode;
+            tail = newNode;
+            newNode.next = head;
+            return;
+        }
+
+        tail.next = newNode;
+        newNode.next = head;
+        tail = newNode;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        createNewList(arr);
+        printList();
+
+        insertAtHead(0);
+        printList();
+
+        insertAtTail(5);
+        printList();
+    }
 }
---
+++ Trees/BinaryTree.java
@@ -0,0 +1,100 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Node{
+    int data;
+    Node left, right;
+
+    Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class BinaryTree {
+    Node root;
+    Scanner sc = new Scanner(System.in);
+
+    Node createNode(){
+
+        int data = sc.nextInt();
+
+        if(data == -1) return null;
+
+        Node newNode = new Node(data);
+
+        System.out.println("Enter left Node: ");
+        newNode.left = createNode();
+
+        System.out.println("Enter right Node: ");
+        newNode.right = createNode();
+
+        return newNode;
+    }
+    
+    void inorder(Node node){
+        if(node != null){
+            inorder(node.left);
+            System.out.print(node.data + " -> ");
+            inorder(node.right);
+        }
+    }
+
+    void preorder(Node node){
+        if(node != null){
+            System.out.print(node.data + " -> ");
+            preorder(node.left);
+            preorder(node.right);
+        }
+    }
+
+    void postorder(Node node){
+        if(node != null){
+            postorder(node.left);
+            postorder(node.right);
+            System.out.print(node.data + " -> ");
+        }
+    }
+
+    void levelorder(Node node){
+        Queue<Node> q = new LinkedList<>();
+
+        if(node == null) return;
+        
+        q.add(node);
+
+        while(!q.isEmpty()){
+            Node current = q.poll();
+            System.out.print(current.data + " -> ");
+
+            if(current.left != null) q.add(current.left);
+
+            if(current.right != null) q.add(current.right);
+        }
+
+    }
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = tree.createNode();
+
+        System.out.println("Inorder: ");
+        tree.inorder(tree.root);
+        System.out.println();
+
+        System.out.println("Preorder: ");
+        tree.preorder(tree.root);
+        System.out.println();
+
+        System.out.println("Postorder: ");
+        tree.postorder(tree.root);
+        System.out.println();
+
+        System.out.println("Levelorder: ");
+        tree.levelorder(tree.root);
+        System.out.println();
+
+    }
+}
